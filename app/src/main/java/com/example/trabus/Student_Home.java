a433e4926c2a88a91d87fe8e023b404064e54d3e
mrawais123 <awaisullalh@gmail.com>
2021-07-10 02:29:35 +0500

    create student fragment

--- app/src/main/java/com/example/trabus/Student_Home.java
+++ app/src/main/java/com/example/trabus/Student_Home.java
@@ -12,9 +12,15 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.trabus.Driver_Navigation_fragment.ProfileFragment;
+import com.example.trabus.Student_Navigation_fragments.ChangePassword;
+import com.example.trabus.Student_Navigation_fragments.Home;
+import com.example.trabus.Student_Navigation_fragments.Logout;
 import com.example.trabus.Student_Navigation_fragments.Profile;
+import com.example.trabus.Student_Navigation_fragments.Reminder;
 import com.google.android.material.snackbar.Snackbar;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
@@ -26,12 +32,13 @@ import com.google.android.material.navigation.NavigationView;
 
 import org.jetbrains.annotations.NotNull;
 
-public class Student_Home extends AppCompatActivity  {
+public class Student_Home extends AppCompatActivity {
     DrawerLayout drawerLayout;
     Toolbar toolbar;
     NavigationView navigation;
     TextView heading;
     ImageView notification;
+
 
     @Override
     public void onBackPressed() {
@@ -58,7 +65,7 @@ public class Student_Home extends AppCompatActivity  {
         drawerLayout.addDrawerListener(drawerToggle);
         drawerToggle.syncState();
         toolbar.setNavigationIcon(R.drawable.menu);
-
+        getSupportFragmentManager().beginTransaction().replace(R.id.RL_student_home,new Home()).commit();
         navigation.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull @NotNull MenuItem item) {
@@ -68,6 +75,30 @@ public class Student_Home extends AppCompatActivity  {
                     case R.id.nav_profile_student:
                         getSupportFragmentManager().beginTransaction().replace(R.id.RL_student_home,new Profile()).commit();
                         heading.setText("Update Profile");
+                        notification.setVisibility(View.INVISIBLE);
+
+                        break;
+                    case R.id.nav_setreminder_student:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.RL_student_home,new Reminder()).commit();
+                        heading.setText("Set Reminder");
+                        notification.setVisibility(View.INVISIBLE);
+
+                        break;
+                    case R.id.nav_logout_student:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.RL_student_home,new Logout()).commit();
+                        heading.setText("Logout");
+                        notification.setVisibility(View.INVISIBLE);
+
+                        break;
+                    case R.id.nav_change_password_student:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.RL_student_home,new ChangePassword()).commit();
+                        heading.setText("Change Password");
+                        notification.setVisibility(View.INVISIBLE);
+
+                        break;
+                    case R.id.nav_home_student:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.RL_student_home,new Home()).commit();
+                        heading.setText("Student Home");
                         notification.setVisibility(View.INVISIBLE);
 
                         break;
---
+++ app/src/main/java/com/example/trabus/Student_Navigation_fragments/ChangePassword.java
@@ -0,0 +1,24 @@
+package com.example.trabus.Student_Navigation_fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.trabus.R;
+
+
+public class ChangePassword extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_change_password, container, false);
+        return  view;
+    }
+}
